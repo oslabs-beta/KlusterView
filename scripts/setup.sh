6631090a855d63570066eced36a0fd603fd5fd84
Kyle Slugg <kyle.slugg@gmail.com>
2023-06-01 19:16:05 -0400

    Now with deployment labels...

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -8,15 +8,15 @@ echo $parentdir
 
 
 echo "Ensuring namespace monitoring exists..."
-kubectl apply namespace monitoring
+kubectl delete namespace monitoring --ignore-not-found
+kubectl create namespace monitoring
+
+echo "Enabling Kube State Metrics..."
+kubectl apply -f $parentdir/deployment/kube_state_metrics/
 
 echo "Setting up Prometheus..."
-kubectl apply -f $parentdir/deployment/clusterRole.yaml
-kubectl apply -f $parentdir/deployment/config-map.yaml
-kubectl apply  -f $parentdir/deployment/prometheus-deployment.yaml 
-kubectl apply -f $parentdir/deployment/prometheus-service.yaml --namespace=monitoring
+kubectl apply -f $parentdir/deployment/prometheus/
+# kubectl apply -f $parentdir/deployment/prometheus-service.yaml --namespace=monitoring
 
 echo "Setting up Grafana..."
-kubectl apply -f $parentdir/deployment/grafana-datasource-config.yaml
-kubectl apply -f $parentdir/deployment/deployment.yaml
-kubectl apply -f $parentdir/deployment/service.yaml
+kubectl apply -f $parentdir/deployment/grafana/
