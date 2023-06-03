02a505435cc692b5c7c9270afde702762bdf4782
24juice22 <mike.james.nunn@gmail.com>
2023-06-03 08:58:42 -0400

    Add Functionality to page, fetch request for pod names, incorporate TypeScript

--- client/components/Sidebar/Sidebar.tsx
+++ client/components/Sidebar/Sidebar.tsx
@@ -1,18 +1,70 @@
-import React from 'react';
+import React, { useEffect, FC, SyntheticEvent } from 'react';
 import { Link } from 'react-router-dom';
 import './Sidebar.scss';
 
-const Sidebar = () => {
-  type Data = {
-    name: string;
+interface SidebarProps {
+  setPodTitle: (podTitle: string) => void;
+  setUrl: (url: string) => void;
+  url: string;
+  klusterUrl: string;
+  podNames: { name: string }[];
+  setPodNames: (podNames) => void;
+}
+
+const Sidebar: FC<SidebarProps> = ({
+  setPodTitle,
+  setUrl,
+  url,
+  klusterUrl,
+  podNames,
+  setPodNames,
+}) => {
+  const getPodNames = async () => {
+    // try {
+    //   const res = await fetch('/grafana/pods');
+    //   const pods = await res.json();
+    //   setPodNames(pods);
+    // } catch (error) {
+    //   console.log(error);
+    // }
+    setPodNames([{ name: '1' }, { name: '2' }, { name: '3' }]);
   };
 
-  const data: Data[] = [{ name: 'One' }, { name: 'Two' }, { name: 'Three' }];
+  useEffect(() => {
+    getPodNames();
+  }, []);
 
-  const podlinks = data.map(function (pod: { name: string }) {
+  const handleKlusterLink = () => {
+    setUrl(klusterUrl);
+  };
+
+  const handlePodLink = (e: SyntheticEvent) => {
+    // Update page title with pod name
+    const podName = e.target.classList[1];
+    const podNumber = podName[podName.length - 1];
+    setPodTitle(podName);
+
+    //update url by inserting current pod number
+    const urlIndexStart = url.indexOf('id/') + 3;
+    const urlIndexEnd = url.indexOf('/', urlIndexStart);
+    let urlStart = url.slice(0, urlIndexStart);
+    let urlEnd = url.slice(urlIndexEnd);
+    let newUrl = urlStart.concat(podNumber).concat(urlEnd);
+    setUrl(newUrl);
+  };
+
+  const podlinks: JSX.Element[] = podNames.map(function (pod: {
+    name: string;
+  }) {
     return (
       <li className='navlink navlink-dropdown'>
-        <Link to='/pods'>{pod.name}</Link>
+        <Link
+          className={`link Pod-${pod.name}`}
+          to='/pods'
+          onClick={handlePodLink}
+        >
+          {pod.name}
+        </Link>
       </li>
     );
   });
@@ -21,10 +73,22 @@ const Sidebar = () => {
     <nav className='sidebar'>
       <ul className='sidebar-list'>
         <li className='navlink'>
-          <Link to='/'>METRICS</Link>
+          <Link className='link' to='/' onClick={handleKlusterLink}>
+            KLUSTER
+          </Link>
         </li>
         <li className='navlink'>
-          <Link to='/pods'>PODS</Link>
+          <Link className='link' to='/'>
+            NODE MAP
+          </Link>
+        </li>
+        <li className='navlink'>
+          <Link className='link' to='/'>
+            ALERTS
+          </Link>
+        </li>
+        <li className='navlink'>
+          <p className='link link-p'>PODS</p>
           <ul className='sidebar-list dropdown-content'>{podlinks}</ul>
         </li>
       </ul>
