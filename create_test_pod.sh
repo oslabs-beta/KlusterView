a47e1029b7ac11e68ab9f8d91a8d49b5b6808fee
Kyle Slugg-Urbino <35903887+kyleslugg@users.noreply.github.com>
2023-06-05 08:55:28 -0400

    Revised startup script (#9)
    * Initial commit
    
    * Initial Commit
    
    * Added necessary imports and configs for server file, edited scripts, added packages, created server folders, added types file
    
    * Added tentative script for setup
    
    * Updated setup script
    
    * Further updates to setup.sh; example dashboard
    
    * created boilerplate routers and controllers
    
    * Added starter folders and files for frontend
    
    * Updated deployment manifest to enable embedding
    
    * Reorganized scripts
    
    * Reorganized scripts
    
    * Cleaned up stray utility files
    
    * Cleaned up stray utility files
    
    * Refactoring of startup scripts
    
    * Refactoring of startup scripts
    
    * Rolled back Grafana deployment
    
    * Rolled back Grafana deployment
    
    * Now with deployment labels...
    
    * Now with deployment labels...
    
    * Revamped setup scripts to include Kube State Metrics
    
    * Removed old PID text files
    
    ---------
    
    Co-authored-by: Shahmar Aliyev <shahmaraliyev@Shahmars-MacBook-Pro.local>
    Co-authored-by: Shahmar Aliyev <64520371+ShahmarAliyev@users.noreply.github.com>
    Co-authored-by: 24juice22 <mike.james.nunn@gmail.com>

--- create_test_pod.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl create deployment kuard --image=gcr.io/kuar-demo/kuard-amd64:blue
-kubectl expose deployment kuard --type=NodePort --port=8080
-kubectl port-forward service/kuard 8080:8080 &
---
+++ scripts/create_test_pod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+kubectl create deployment kuard --image=gcr.io/kuar-demo/kuard-amd64:blue
+kubectl expose deployment kuard --type=NodePort --port=8080
+kubectl wait --for=condition=Ready pod/$(kubectl get pod -l app=kuard -n monitoring -o jsonpath="{.items[0].metadata.name}") -n monitoring
+kubectl port-forward service/kuard 8080:8080 &
+
+#Write process ID to file
