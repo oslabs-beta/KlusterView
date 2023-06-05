6c59b502c1ab9db1a133c0ca683faa3d9b4f5841
Jonathan Tsai <jonathantsai@jonathans-mbp.mynetworksettings.com>
2023-06-05 10:07:58 -0400

    finished prom routers

--- server/routes/promRouter.ts
+++ server/routes/promRouter.ts
@@ -1,5 +1,15 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import promController from '../controllers/promController';
 
 const promRouter = express.Router();
+
+promRouter.get(
+  '/pods/',
+  promController.getPodNames,
+  promController.getpodIP,
+  (req: Request, res: Response) => {
+    return res.status(200).json(res.locals);
+  }
+);
 
 export default promRouter;
