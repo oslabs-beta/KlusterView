90939d1b51b02088c703da37c80c6715c3db9141
Shahmar Aliyev <shahmaraliyev@Shahmars-MacBook-Pro.local>
2023-06-10 14:23:17 -0400

    created endpoints to access pod names with their status and their nodes

--- server/routes/promRouter.ts
+++ server/routes/promRouter.ts
@@ -12,4 +12,19 @@ promRouter.get(
   }
 );
 
+promRouter.get(
+  '/pod/status',
+  promController.getPodStatuses,
+  (req: Request, res: Response) => {
+    return res.status(200).json(res.locals.podStatusNames);
+  }
+);
+promRouter.get(
+  '/pods/nodes',
+  promController.getPodNodes,
+  (req: Request, res: Response) => {
+    return res.status(200).json(res.locals.podNodes);
+  }
+);
+
 export default promRouter;
