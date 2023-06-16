f522ce70629359eca042ca7640327de6f0138563
24juice22 <mike.james.nunn@gmail.com>
2023-06-16 10:43:48 -0400

    testing updates

--- __tests__/frontend-react.test.ts
+++ __tests__/frontend-react.test.ts
@@ -1,75 +1,56 @@
+/**
+ * @jest-environment jsdom
+ */
 import '@testing-library/jest-dom';
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, cleanup } from '@testing-library/react';
 import Header from '../client/components/Header/Header';
-import Sidebar from '../components/Sidebar/Sidebar';
+import Sidebar from '../client/components/Sidebar/Sidebar';
 import { Routes, Route } from 'react-router-dom';
+import { getNodeIPs } from '../server/controllers/initializationController';
+import Dashboard from '../client/components/Dashboard/Dashboard';
 
+const IPList = getNodeIPs();
+const GRAF_IP = IPList[0];
+
+afterEach(() => {
+  cleanup();
+});
 
 describe('Header', () => {
-  it('renders the header with logo and name', () => {
-
-    header = render(<Header >);
-
-
+  test('renders the header with logo and name', () => {
+    render(<Header />);
     const logo = screen.getByText('KV');
-    const name = screen.getByText('Kluster View');
-
+    const name1 = screen.getByText('Kluster');
+    const name2 = screen.getByText('View');
     expect(logo).toBeInTheDocument();
-    expect(name).toBeInTheDocument();
+    expect(name1).toBeInTheDocument();
+    expect(name2).toBeInTheDocument();
   });
 });
 
-describe('Sidebar', () => {
-    it('renders the sidebar with link', () => {
-     
-      render(
- 
-          <Sidebar
-            setPodTitle={() => {}}
-            setUrl={() => {}}
-            url=""
-            podsUrl=""
-            setPodsUrl={() => {}}
-            klusterUrl=""
-            allPodsUrl=""
-            podInfo={[
-              { name: 'Pod1', ip: 123 },
-              { name: 'Pod2', ip: 456 }
-            ]}
-            setPodInfo={() => {}}
-            nodeMapInfo={{}}
-          />
+describe('Dashboard', () => {
+  test('renders the dashboard component', () => {
+    //somehow get a test url
+    console.log('Graf ip', GRAF_IP);
+    const testUrl = `http://admin:admin@${GRAF_IP}:32000/api/search?type=dash-db`;
 
-      );
-  
-     
-      const klusterLink = screen.getByRole('link', { name: /kluster/i });
-      const podsLink = screen.getByRole('link', { name: /pods/i });
-      const nodeMapLink = screen.getByRole('link', { name: /node map/i });
-  
- 
-      expect(klusterLink).toBeInTheDocument();
-      expect(podsLink).toBeInTheDocument();
-      expect(nodeMapLink).toBeInTheDocument();
-    });
+    const { container } = render(<Dashboard url={testUrl} />);
+    console.log('container', container.firstChild);
 
-    describe('Dashboard', () => {
-        test('renders the dashboard component', () => {
-          //?
-          const testUrl = 'http://admin:admin@${GRAF_IP}:${GRAF_NODE_PORT}/api/search?type=dash-db'
-      
+    // const dashboardContainer = screen.getByTestId('dashboard-container');
+    // expect(dashboardContainer).toBeInTheDocument();
+    console.log('classlist', container.classList);
+    expect(container.firstChild.classList.contains('dashboard-container')).toBe(
+      true
+    );
 
-          render(<Dashboard-url={testUrl} />);
-      
+    // const iframeElement = screen.getByTestId('dashboard');
+    // expect(iframeElement).toBeInTheDocument();
+    expect(
+      container.firstChild.firstChild.classList.contains('dashboard')
+    ).toBe(true);
 
-          const dashboardContainer = screen.getByTestId('dashboard-container');
-          expect(dashboardContainer).toBeInTheDocument();
-      
-
-          const iframeElement = screen.getByTestId('dashboard-iframe');
-          expect(iframeElement).toBeInTheDocument();
-          expect(iframeElement).toHaveAttribute('src', testUrl);
-        });
-      
-      });
+    // expect(iframeElement).toHaveAttribute('src', testUrl);
+  });
+});
