126731e9c78f817d367d71a731f01854ea733f68
Kyle Slugg <kyle.slugg@gmail.com>
2023-06-23 19:08:55 -0400

    Removed _util_scripts

--- _util_scripts/update_docker_img.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo docker build . -t kyleslugg/klusterview
-sudo docker push kyleslugg/klusterview:latest
-echo "Pushed updated image to kyleslugg/klusterview"
-
-sudo docker build . -t kyleslugg/klusterview-dev -f Dockerfile-dev
-sudo docker push kyleslugg/klusterview-dev:latest
-echo "Pushed updated image to kyleslugg/klusterview-dev"
--- _util_scripts/update_kv_k8.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-kubectl delete service klusterview -n monitoring-kv
-kubectl delete deployment klusterview -n monitoring-kv
-kubectl apply -f ../klusterview_manifest.yaml
